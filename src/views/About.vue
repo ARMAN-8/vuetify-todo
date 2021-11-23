72ae3315099dc8f08bb459cabe8d58de3e92defd
ARMAN-8 <alimnurarman@gmail.com>
2021-11-23 10:23:36 +0600

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about pa-6">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis dolorem ex expedita ipsa minima
+      repellendus tempora temporibus veritatis voluptatum.
+    </p>
+  </div>
+</template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home pa-6">
+    Todo page
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+  }
+</script>
